b78b24e8f82695c800fa2c2246217758bdacd9a0
raphavtorres <raphaeltorresprofissional@gmail.com>
2025-03-08 14:24:22 -0300

    chore: remove forceSwcTransforms

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,6 @@ const nextConfig = {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
-  experimental: {
-    forceSwcTransforms: true, // Enable SWC transforms
-  },
 };
 
 export default nextConfig;
